61b06756509357734bedeabc9f7ea3beebb593fb
mario <emailoftheyear@gmail.com>
2013-10-13 16:36:32 +0100

    Changes on ud

--- urbandictionary/urbandictionary.py
+++ urbandictionary/urbandictionary.py
@@ -9,6 +9,7 @@ import mylib
 # curl http://api.urbandictionary.com/v0/define\?term\=yolo | python -mjson.tool
 
 URL = 'http://api.urbandictionary.com/v0/define?%s'
+USAGE = ".ud: find argument required"
 
 def str_is_int(s):
     try:
@@ -18,26 +19,33 @@ def str_is_int(s):
         return (False, s)
 
 if __name__=="__main__" and len(sys.argv) > 1:
-	p, n = str_is_int(sys.argv[-1])
+   p, n = str_is_int(sys.argv[-1])
 
-	if p :
-		q = " ".join(sys.argv[1:-1])
-	else :
-		n, q = 1, " ".join(sys.argv[1:])
-	
-	par = urllib.urlencode({'term': q })
-	res = urllib2.urlopen(urllib2.Request(URL % par))
+   if p :
+      q = " ".join(sys.argv[1:-1])
+   else :
+      n, q = 1, " ".join(sys.argv[1:])
+   
+   par = urllib.urlencode({'term': q })
+   res = urllib2.urlopen(urllib2.Request(URL % par))
 
-	res = json.load(res)
-	typ = res[u"result_type"]
+   res = json.load(res)
+   typ = res[u"result_type"]
 
-	if typ == "exact" :
-		try:
-			l = len(res[u"list"])
-			res = res[u"list"][n - 1][u"definition"]
-			res = res.replace("\r\n", " ")
-			mylib.print_console("\002%d/%d\002  %s" % (n,l,res))
-		except IndexError :
-			pass
-
+   if typ == "exact" :
+      try:
+         if n <= 0:
+           n = 1
+         l = len(res[u"list"])
+         res = res[u"list"][n - 1][u"definition"]
+         res = res.replace("\r\n", " ")
+         if l > 1 and n < l:
+            mylib.print_console("%d found '.ud %s %d' for the next one " % (l,q,n+1))
+         mylib.print_console("%s" % res)
+      except IndexError :
+         mylib.print_console("Not found")
+   else:
+      mylib.print_console("Not found")
+else:
+    mylib.print_console(USAGE);
 # vim: ts=4:sw=4
