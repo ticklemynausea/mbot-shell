4e6cd454818ee2232e1a240777673bb4f1629c2f
b0nk <fsantos.pt@hotmail.com>
2014-02-05 03:10:30 +0000

    different method to get humble titles

--- humblebundle/humblebundle.py
+++ humblebundle/humblebundle.py
@@ -6,8 +6,9 @@ import bs4
 # ../mylib.py
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from mylib import print_console
+from mylib import strip as stripHTML
 
 soup  = bs4.BeautifulSoup(requests.get("http://humblebundle.com").text)
-games = [img["alt"] for img in soup.select("ul.game-boxes li img.game-box")]
+games = [str.strip(stripHTML(str(i))) for i in soup.find_all('span', 'item-title')]
 
 print_console(", ".join(games))
