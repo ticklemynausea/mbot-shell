97b93f9a7b25d0547cb046dc523be49a82996a31
mario <emailoftheyear@gmail.com>
2014-03-20 16:07:03 +0000

    Fixed for the new preev api

--- btc/btc.sh
+++ btc/btc.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
- 
-btce_price() {
-  python -c 'import json; import sys; print json.load(sys.stdin)["markets"]["btce"]["price"]'
+
+btce_price_eur() {
+  python -c 'import json; import sys; print json.load(sys.stdin)["btc"]["eur"]["btce"]["last"]'
 }
- 
+
+btce_price_usd() {
+  python -c 'import json; import sys; print json.load(sys.stdin)["btc"]["usd"]["btce"]["last"]'
+}
+
 AMOUNT=${1:-1.0}
- 
-USD=$(curl -sf http://preev.com/pulse/source:btce/unit:btc,usd | btce_price)
-EUR=$(curl -sf http://preev.com/pulse/source:btce/unit:btc,eur | btce_price)
- 
+
+USD=$(curl -sf http://preev.com/pulse/units:btc+usd/sources:btce | btce_price_usd)
+EUR=$(curl -sf http://preev.com/pulse/units:btc+eur/sources:btce | btce_price_eur)
+
 USD=$(echo "$USD" "$AMOUNT" | awk '{ printf "%.2f\n", $1 * $2}')
 EUR=$(echo "$EUR" "$AMOUNT" | awk '{ printf "%.2f\n", $1 * $2}')
- 
+
 echo "$AMOUNT btc = $USD usd = $EUR eur"
