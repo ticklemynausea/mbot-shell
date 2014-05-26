7098e54a24470ea3d390eeee8b5997a0c0214f3d
b0nk <fsantos.pt@hotmail.com>
2014-05-26 20:47:49 +0100

    added 4chan search

---
+++ imageboards/s4chan.py
@@ -0,0 +1,76 @@
+import os, sys
+import urllib, json
+
+# ../mylib.py
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from mylib import unescape, strip, print_console
+
+logo = '3::54chan'
+
+def man():
+  print_console("Usage: !4chan <board> <search terms>")
+
+def format(comment):
+
+  comment = unescape(comment)
+  comment = comment.replace('<br>', ' ')
+  comment = comment.replace('<wbr>', '')
+  comment = comment.replace('<span class="quote">', '3') #greentext open
+  comment = comment.replace('<span class="deadlink">', '3') #greentext open
+  comment = comment.replace('</span>', '') #close color
+  comment = strip(comment) #remove the rest of html tags
+  
+  return comment
+
+def search(board, search):
+  res = []
+
+  try:
+    catalog = json.load(urllib.urlopen('https://a.4cdn.org/%s/catalog.json' % board))
+  
+    for i in catalog:
+      for j in i['threads']:
+        if search.lower() in j.get('sub', '').lower() or search.lower() in j.get('com', '').lower():
+          subject = j.get('sub', 'Empty subject')
+          subject = unescape(subject)
+          post = j.get('com', 'Empty post')
+          post = format(post)
+  
+          if len(post) > 100:
+            post = post[:100] + '...' #close color here also
+            
+          boardLink = 'https://boards.4chan.org/%s/thread/%s' % (board, j['no'])
+  
+          text = '%s /%s/ | %s | %s | %s (R:%s, I:%s)' % (logo, board, subject, post, boardLink, j['replies'], j['images'])
+          res.append(text)
+    return res
+  except(IOError):
+    return ['%s Error: Try again later' % logo]
+
+def getValidBoards():
+  boards = []
+
+  l = json.load(urllib.urlopen('https://a.4cdn.org/boards.json'))
+
+  for i in l['boards']:
+    boards.append(str(i['board']))
+
+  return boards
+
+if len(sys.argv) < 3:
+  man()
+  exit(1)
+
+board = sys.argv[1]
+terms = " ".join(sys.argv[2:])
+
+if board not in getValidBoards():
+  print_console("/%s/ is not a real board" % board)
+  exit(1)
+
+res = search(board, terms)
+if res:
+  for i in res[:2]:
+    print_console(i)
+else:
+  print_console("No results for %s on /%s/" % (terms, board))
