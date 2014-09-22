a1ff093642c92a0033b400b23795bb219e9e8b06
b0nk <fsantos.pt@hotmail.com>
2014-09-22 01:53:13 +0100

    updated euromilhoes
    - Changed the source of the result numbers
    - Script is now smaller and faster

--- euromilhoes/euromilhoes.py
+++ euromilhoes/euromilhoes.py
@@ -1,3 +1,5 @@
+# coding=utf-8
+
 import os
 import sys
 import requests
@@ -6,108 +8,41 @@ import bs4
 # ../mylib.py
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from mylib import print_console
-from mylib import strip as stripHTML
+
 
 def man():
-  print_console('Usage: .euromilhoes | .totoloto | .joker') # ~:python euromilhoes.py [e | t | j]
+  # ~:python euromilhoes.py [e | t | j]
+  print_console('Usage: .euromilhoes | .totoloto | .joker')
   exit(-1)
 
 
 def getResults(c):
 
-  # EUROMILHOES
-  emInfoStyle = 'border-bottom:1px solid white; background-color:#e2e2e2;padding:2px; padding-left:6px;\\'
+  # Pick source
+  if c is 'e':
+    soup = bs4.BeautifulSoup(requests.get('https://www.jogossantacasa.pt/web/SCCartazResult/euroMilhoes').text)
+    game = u"Euromilhões"
+  if c is 't':
+    soup = bs4.BeautifulSoup(requests.get('https://www.jogossantacasa.pt/web/SCCartazResult/totolotoNew').text)
+    game = u"Totoloto"
+  if c is 'j':
+    soup = bs4.BeautifulSoup(requests.get('https://www.jogossantacasa.pt/web/SCCartazResult/joker').text)
+    game = u"Joker"
 
-  emNumStyle = 'font-weight:bold; width: 25px; height: 25px; background-position:center; background-repeat:no-repeat; text-align:center; vertical-align:middle; background-image:url(/_media/img/2008/Jan/tmpl_fe_fundo_numero_on_ufuv.jpg);padding:2px; '
+  # Contest date
+  dateInfo = soup.find('span', class_='dataInfo').stripped_strings
+  date = " | ".join(dateInfo)
 
-  emStarStyle = 'padding:2px;font-weight:bold; width: 27px; height: 25px; background-position:center; background-repeat:no-repeat; text-align:center; vertical-align:middle; background-image:url(/_media/img/2008/Jan/tmpl_fe_fundo_estrela_on_dwma.jpg); '
+  # The numbers
+  numberInfo = soup.find('ul', class_='colums').find('li').text
 
-  emNum1 = []
-  emNum2 = []
-  emInfo1 = ''
-  emInfo2 = ''
-
-  # TOTOLOTO
-  totInfoStyle = 'border-bottom:1px solid white; background-color:#e2e2e2;padding:2px; padding-left:6px;'
-
-  totStarStyle = 'padding:2px;font-weight:bold; width: 27px; height: 25px; background-position:center; background-repeat:no-repeat; text-align:center; vertical-align:middle; background-image:url(/_media/img/2008/Jan/tmpl_fe_fundo_estrela_on_dwma2.jpg); '
-
-  totNum1 = []
-  totNum2 = []
-  totInfo1 = ''
-  totInfo2 = ''
-
-  # JOKER
-  jokerStyle = 'padding:8px; padding-left:6px; font-weight:bold; border-bottom:1px solid white; background-color:white;  background-repeat:repeat-x; background-image:url(http://www.euromilhoes.com/_include/images/fundo_row.png); color:#666666; font-size:14px; color:#990000; text-align:center;'
-
-  jokerInfo = ''
-  
-  # Get the results
-  soup = bs4.BeautifulSoup(requests.get('http://www.euromilhoes.com/index_intro.php').text)
-
-  res = soup.findAll('td', {'style': emInfoStyle}) # euromilhoes info
-  emInfo1 = str.strip(stripHTML(str(res[0])))
-  emInfo1 = emInfo1[:23] + ' ' + emInfo1[23:]
-  emInfo2 = str.strip(stripHTML(str(res[1])))
-  emInfo2 = emInfo2[:23] + ' ' + emInfo2[23:]
-
-
-  res = soup.findAll('td', {'style': emNumStyle}) # all numbers (no stars)
-  for i in res[:5]:
-    emNum1.append(str.strip(stripHTML(str(i))))
-  for i in res[5:10]:
-    emNum2.append(str.strip(stripHTML(str(i))))
-  for i in res[10:15]:
-    totNum1.append(str.strip(stripHTML(str(i))))
-  for i in res[15:20]:
-    totNum2.append(str.strip(stripHTML(str(i))))
-
-  
-  res = soup.findAll('td', {'style': emStarStyle}) # euromilhoes stars
-  emNum1.append(str.strip(stripHTML(str(res[0]) + '*')))
-  emNum1.append(str.strip(stripHTML(str(res[1]) + '*')))
-  emNum2.append(str.strip(stripHTML(str(res[2]) + '*')))
-  emNum2.append(str.strip(stripHTML(str(res[3]) + '*')))
-
-
-  res = soup.findAll('td', {'style': totInfoStyle}) # totoloto info and joker(last)
-  totInfo1 = str.strip(stripHTML(str(res[0])))
-  totInfo1 = totInfo1[:23] + ' ' + totInfo1[23:]
-  totInfo2 = str.strip(stripHTML(str(res[1])))
-  totInfo2 = totInfo2[:23] + ' ' + totInfo2[23:]
-  #joker
-  jokerInfo = str.strip(stripHTML(str(res[2])))
-  jokerInfo = jokerInfo[:23] + ' ' + jokerInfo[23:]
-
-
-  res = soup.findAll('td', {'style': totStarStyle}) # totoloto stars
-  totNum1.append(str.strip(stripHTML(str(res[0]) + '*')))
-  totNum2.append(str.strip(stripHTML(str(res[1]) + '*')))
-
-
-  res = soup.findAll('td', {'style': jokerStyle}) # joker number
-  joker = str.strip(stripHTML(str(res[0])))
-
-  if (c == 'e'): # Euromilhoes
-    print_console(emInfo1)
-    print_console(', '.join(emNum1))
-    print_console(emInfo2)
-    print_console(', '.join(emNum2))
-
-  if (c == 't'): # Totoloto
-    print_console(totInfo1)
-    print_console(', '.join(totNum1))
-    print_console(totInfo2)
-    print_console(', '.join(totNum2))
-
-  if (c == 'j'): # Joker
-    print_console(jokerInfo)
-    print_console(joker)
+  # Output
+  print_console('%s: %s - %s' % (game, numberInfo, date))
 
 
 if len(sys.argv) < 2:
   man()
-  
+
 contest = sys.argv[1]
 if contest in ['e', 't', 'j']:
   getResults(contest)
