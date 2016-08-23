5c853d6bef25cb4a7a341300df53fa7101afdae2
Hugo Peixoto <hugo.peixoto@gmail.com>
2016-08-23 14:42:54 +0100

    Fixes "sugestes" string

--- priberam/priberam.py
+++ priberam/priberam.py
@@ -74,7 +74,7 @@ def procura(pesquisa, indice = 1):
     resultado = ''.join(resultado)
     resultado = ' '.join([w.strip() for w in resultado.split('\n')])
 
-    mylib.print_console('%s sugestes:%s.' % (pesquisa, resultado))
+    mylib.print_console('Sugestões para %s:%s.' % (pesquisa, resultado))
     return
 
   if indice > contagem :
