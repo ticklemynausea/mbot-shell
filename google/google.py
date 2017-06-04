766acafb367afb607f73643c89a8346ee5397c64
b0nk <fsantos.pt@hotmail.com>
2017-06-04 17:28:19 +0100

    fixed unicode troubles

--- google/google.py
+++ google/google.py
@@ -15,28 +15,29 @@ from mylib import print_console, unescape, strip
 # Free API limited to 100 searches per day (lol)
 
 
-logo = u"2G4o8o2g3l4e"
+logo = "2G4o8o2g3l4e"
 
 if len(sys.argv) < 2:
   print_console("%s search syntax: !google <terms>" % logo);
   exit(-1)
 
-query = unicode(u" ".join(sys.argv[1:]))
+arg = [i.decode(sys.getfilesystemencoding()) for i in sys.argv[1:]]
+query = u" ".join(arg)
 
-string = u"%s %s: " % (logo, query)
-query = urllib.urlencode ({ u'q' : query })
-baseURL = u"https://www.googleapis.com/customsearch/v1?key=%s&cx=%s&alt=json&" % (CSE_API_KEY, ENGINE_ID)
+string = "%s %s: " % (logo, query)
+query = urllib.urlencode ({ 'q' : query.encode(sys.getfilesystemencoding()) })
+baseURL = "https://www.googleapis.com/customsearch/v1?key=%s&cx=%s&alt=json&" % (CSE_API_KEY, ENGINE_ID)
 response = urllib.urlopen(baseURL + query).read()
 json = m_json.loads(response)
 results = json.get('items', None)
 
 if results:
   for result in results[:3]:
-    title = unicode(result['title'])
-    url = unicode(result['link'])
-    string = unicode(string) + u"%s ( %s ); " % (title, url)
+    title = result['title']
+    url = result['link']
+    string = string + "%s ( %s ); " % (title, url)
 
-  print_console(u"%s: %s" % (string, url))
+  print_console("%s: %s" % (string, url))
 
 else:
   print_console("No results!")
