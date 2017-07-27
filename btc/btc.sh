bda3369dca7aeed604441698ea0ca0f95285cdac
LeonelF <leonelf@gmail.com>
2017-07-27 14:35:40 +0100

    Update btc.sh
    After some requests I decided to provide on the output the price in Escudos (old currency in Portugal)

--- btc/btc.sh
+++ btc/btc.sh
@@ -20,9 +20,28 @@ price_usd() {
 AMOUNT=${2:-1.0}
 
 EUR=$(curl -sf "https://min-api.cryptocompare.com/data/pricemultifull?fsyms=$COIN&tsyms=EUR" | price_eur)
+CONTOS=$(curl -sf "https://min-api.cryptocompare.com/data/pricemultifull?fsyms=$COIN&tsyms=EUR" | price_eur)
 USD=$(curl -sf "https://min-api.cryptocompare.com/data/pricemultifull?fsyms=$COIN&tsyms=USD" | price_usd)
 
 EUR=$(echo "$EUR" "$AMOUNT" | awk '{ printf "%.2f\n", $1 * $2}')
 USD=$(echo "$USD" "$AMOUNT" | awk '{ printf "%.2f\n", $1 * $2}')
+CONTOS=$(echo "$CONTOS" "$AMOUNT" | awk '{ printf "%.2f\n", (($1 * $2)*200.482)/1000}')
+CONTOSINT=${CONTOS%.*}
+ESCUDOS=$(echo "$CONTOS" "$CONTOSINT" | awk '{ printf "%.3f\n", ($1 - $2)*1000}')
+ESCUDOS=${ESCUDOS%.*}
 
-echo "$AMOUNT $COIN = $USD usd   $AMOUNT $COIN = $EUR eur"
+if [ "$CONTOSINT" -ne "0" ]
+then
+  CONTOSSTR="$CONTOSINT Contos de rei"
+fi
+
+if [ "$ESCUDOS" -ne "0" ]
+then
+  if [ "$CONTOSINT" -ne "0" ]
+  then
+  ESCUDOSSTR=" e $ESCUDOS escudos"
+  else
+  ESCUDOSSTR="$ESCUDOS escudos"
+  fi
+fi
+echo "$AMOUNT $COIN = $USD Usd   $AMOUNT $COIN = $EUR Eur   $AMOUNT $COIN = $CONTOSSTR$ESCUDOSSTR"
