4fb6ce5c2efcfc05a98ea09c59e5da566a959214
LeonelF <leonelf@gmail.com>
2017-07-28 17:10:45 +0100

    Update weather.py
    Fixed, script didn't support cities names with more than one word

--- weather/weather.py
+++ weather/weather.py
@@ -6,12 +6,16 @@ import sys
 reload(sys)  
 sys.setdefaultencoding('utf8')
 import urllib2
+city = ""
 
-city = sys.argv[1]
+for x in range(1, len(sys.argv)):
+	if x == 1:
+		city = sys.argv[x]
+	else:
+		city = city + "%20" + sys.argv[x]
 
 url = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text=%22{}%22)%20and%20u=%22c%22&format=json&env=store://datatables.org/alltableswithkeys'.format(city)
 
-#print(url)
 
 response = urllib2.urlopen(url)
 values = json.loads(response.read())
