43d1418f23fdb09064dd9ab0d2a51a7bb7e1b64d
LeonelF <leonelf@gmail.com>
2017-08-01 09:22:20 +0100

    Update weather.py
    Added error handling

--- weather/weather.py
+++ weather/weather.py
@@ -6,6 +6,7 @@ import sys
 reload(sys)  
 sys.setdefaultencoding('utf8')
 import urllib2
+
 city = ""
 forecast = 0
 if (len(sys.argv) == 1):
@@ -23,8 +24,11 @@ for x in range(1, len(sys.argv)):
 
 url = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text=%22{}%22)%20and%20u=%22c%22&format=json&env=store://datatables.org/alltableswithkeys'.format(city)
 
-
-response = urllib2.urlopen(url, timeout=5)
+try:
+	response = urllib2.urlopen(url, timeout=5)
+except urllib2.URLError, e:
+		print("There was an error: %r" % e)
+		sys.exit(0)
 values = json.loads(response.read())
 if values['query']['results'] is None:
 	print("No results found")
