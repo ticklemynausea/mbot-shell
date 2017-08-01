0484d640cabe921e993565d4b6fb2459a637fcf8
LeonelF <leonelf@gmail.com>
2017-08-01 10:50:30 +0100

    Update weather.py
    Improved the code with some error catching and changed the output of forecast and also added the option to choose the number of days for forecast

--- weather/weather.py
+++ weather/weather.py
@@ -8,9 +8,9 @@ sys.setdefaultencoding('utf8')
 import urllib2
 
 city = ""
-forecast = 0
+forecastnum = 0
 if (len(sys.argv) == 1):
-	print("Usage .w <location> [--forecast]")
+	print("Usage .w <location> [--forecast [1-9]]")
 	sys.exit(0)
 for x in range(1, len(sys.argv)):
 	if x == 1:
@@ -19,8 +19,14 @@ for x in range(1, len(sys.argv)):
 		if sys.argv[x] != "--forecast":
 			city = city + "%20" + sys.argv[x]
 		else:
-			forecast = 1
-		
+			if x == len(sys.argv) - 1:
+				forecastnum = 3
+			else:
+				try:
+					forecastnum = int(sys.argv[x + 1])
+				except:
+					forecastnum = 3
+			break
 
 url = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text=%22{}%22)%20and%20u=%22c%22&format=json&env=store://datatables.org/alltableswithkeys'.format(city)
 
@@ -39,7 +45,11 @@ else:
 	astronomy = channel['astronomy']
 	wind = channel['wind']
 	print("%s Temperature: %s Cº  Condition: %s  Humidity: %s%%  Wind: %s km/h  Sunrise/Sunset: %s/%s" % (channel['item']['title'], conditions['temp'], conditions['text'], atemosphere['humidity'], wind['speed'], astronomy['sunrise'], astronomy['sunset'] ) )
-	if forecast == 1:
+	if forecastnum > 0:
+		if forecastnum > 9:
+			forecastnum = 9
 		forecast = channel['item']['forecast']
-		for x in range(1, len(forecast)):
-			print ("Forecast for %s: High: %s Low: %s Condition: %s" % (forecast[x]['date'], forecast[x]['high'], forecast[x]['low'], forecast[x]['text']))
+		forecastprint = ""
+		for x in range(1, forecastnum + 1):
+			forecastprint = forecastprint + "Forecast for %s: High: %s Low: %s Condition: %s " % (forecast[x]['date'], forecast[x]['high'], forecast[x]['low'], forecast[x]['text'])
+		print (forecastprint)
