69734cf2e8ca6b2ca04e36f9c00193c684d25fe8
LeonelF <leonelf@gmail.com>
2017-08-01 08:46:14 +0100

    Update weather.py
    Added timeout to the url opening command.

--- weather/weather.py
+++ weather/weather.py
@@ -24,7 +24,7 @@ for x in range(1, len(sys.argv)):
 url = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text=%22{}%22)%20and%20u=%22c%22&format=json&env=store://datatables.org/alltableswithkeys'.format(city)
 
 
-response = urllib2.urlopen(url)
+response = urllib2.urlopen(url, timeout=5)
 values = json.loads(response.read())
 if values['query']['results'] is None:
 	print("No results found")
