43970c1bb84649e9c33b4812ffcf862b352a576b
b0nk <fsantos.pt@hotmail.com>
2017-09-27 01:06:25 +0100

    humblebundle: removed mobile bundle / better output

--- humblebundle/humblebundle.py
+++ humblebundle/humblebundle.py
@@ -2,46 +2,53 @@ import os
 import sys
 import requests
 import bs4
+import re
 
 # ../mylib.py
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from mylib import print_console
 
 def man():
-  print_console("Usage: .hb [g]ames | [b]ooks | [m]obile || Default is 'g'")
+  print_console("Usage: .hb [g]ames | [b]ooks || Default is 'g'")
 
 def getBundle(b):
-  charities = ["Electronic Frontier Foundation", "American Red Cross", "Child's Play Charity",
-               "Mozilla Foundation", "CodeNow", "Maker Education Initiative",
-               "Save the Children", "charity: water", "Exclusive Dreamcast T-Shirt",
-               "AbleGamers", "Willow", "SpecialEffect",
-               "GamesAid", "Girls Who Code", "The V Foundation",
-               "buildOn", "The IndieCade Foundation", "Extra Life / Children's Miracle Network Hospitals",
-               "Heifer International", "Comic Book Legal Defense Fund",
-               "More games coming soon!", "More content coming soon!"]
+  charities = [u"Electronic Frontier Foundation", u"American Red Cross", u"Child's Play Charity",
+               u"Mozilla Foundation", u"CodeNow", u"Maker Education Initiative",
+               u"Save the Children", u"charity: water", u"Exclusive Dreamcast T-Shirt",
+               u"AbleGamers", u"Willow", u"SpecialEffect",
+               u"GamesAid", u"Girls Who Code", u"The V Foundation",
+               u"buildOn", u"The IndieCade Foundation", u"Extra Life / Children's Miracle Network Hospitals",
+               u"Heifer International", u"Comic Book Legal Defense Fund",
+               u"More games coming soon!", u"More content coming soon!"]
   items = []
 
-  if b is 'm':
-    soup = bs4.BeautifulSoup(requests.get("https://humblebundle.com/mobile").text, "html.parser")
-  elif b is 'b':
+  color1 = 14
+  color2 = 15
+
+  if b is 'b':
     soup = bs4.BeautifulSoup(requests.get("https://humblebundle.com/books").text, "html.parser")
   else:
     soup = bs4.BeautifulSoup(requests.get("https://humblebundle.com/").text, "html.parser")
 
-  res = soup.find_all('span', 'game-box')
+  res = soup.find_all('div', class_='game-boxes')
 
   if res:
-    bTitle = soup.find('img', class_="promo-logo")['alt']
+    bTitle = unicode(re.search("(.+) \(.+\)", soup.title.text).group(1))
+    colorToggler = True
+
     for i in res:
-      item = i.find('img')['alt']
+      item = unicode(i.find('div', class_='dd-image-box-caption').text.strip())
 
-      if item not in charities and "Soundtrack" not in item:
-        items.append(item)
+      if item not in charities and u"Soundtrack" not in item:
+        game = u"%02d%s" % ((color1 if colorToggler else color2), item)
+        items.append(game)
+        # This toggles the bool
+        colorToggler = not colorToggler
 
-    print_console("07%s: %s" % (bTitle, ", ".join(items)))
+    print_console(u"07%s: %s" % (bTitle, u", ".join(items)))
 
   else:
-    print_console("This bundle is over!")
+    print_console(u"This bundle is over!")
 
 
 try:
@@ -49,7 +56,7 @@ try:
 except(IndexError):
   bundle = 'g'
 
-if bundle in ['g', 'b', 'm']:
+if bundle in ['g', 'b']:
   getBundle(bundle)
 else:
   man()
