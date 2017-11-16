02aa5db8314db508d7ce86a0f9094c428bf74cfe
Mário C <igotbugs@ticklemynausea.net>
2017-11-16 21:21:44 +0000

    Fix inquisicao

--- inquisicao/inquisicao.py
+++ inquisicao/inquisicao.py
@@ -6,10 +6,15 @@ try:
 except ImportError:
     import urllib as urlrequest
 
+# ../mylib.py
 import json
+import sys
+import os
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from mylib import print_console
 
 response = urlrequest.urlopen('https://inquisicao.deadbsd.org/api/degredo')
 data = response.read()
 j = json.loads(data)
 
-print("[%s] Crime: %s | %s" % (j['processo'], j['crime'], j['sentenca']))
+print_console("[%s] Crime: %s | %s" % (j['processo'], j['crime'], j['sentenca']))
