b48346e44936d606119271b0348dd8195ef35c4b
Pedro de Oliveira <falsovsky@gmail.com>
2018-02-27 13:50:30 +0000

    Remove commented crap

--- inquisicao/inquisicao.py
+++ inquisicao/inquisicao.py
@@ -11,9 +11,6 @@ from mylib import print_console
 TIMEOUT = 0.5
 
 request = requests.get('https://inquisicao.deadbsd.org/api/degredo', timeout=TIMEOUT)
-#response = urlrequest.urlopen('https://inquisicao.deadbsd.org/api/degredo',p)
-#data = response.read()
-#j = json.loads(data)
 j = request.json()
 
 print_console("[%s] %s | Crime: %s | %s" % (j['processo'], j['titulo'], j['crime'], j['sentenca']))
