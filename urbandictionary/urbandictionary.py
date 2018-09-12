621efc0464c461daaffdb30b8681d05e6ccb52a0
Mário C <igotbugs@ticklemynausea.net>
2018-09-12 22:11:52 +0100

    Fix urban dictionary script

--- urbandictionary/urbandictionary.py
+++ urbandictionary/urbandictionary.py
@@ -19,33 +19,28 @@ def str_is_int(s):
         return (False, s)
 
 if __name__=="__main__" and len(sys.argv) > 1:
-   p, n = str_is_int(sys.argv[-1])
+    p, n = str_is_int(sys.argv[-1])
 
-   if p :
-      q = " ".join(sys.argv[1:-1])
-   else :
-      n, q = 1, " ".join(sys.argv[1:])
-   
-   par = urllib.urlencode({'term': q })
-   res = urllib2.urlopen(urllib2.Request(URL % par))
+    if p :
+        q = " ".join(sys.argv[1:-1])
+    else :
+        n, q = 1, " ".join(sys.argv[1:])
 
-   res = json.load(res)
-   typ = res[u"result_type"]
+    par = urllib.urlencode({'term': q })
+    res = urllib2.urlopen(urllib2.Request(URL % par))
 
-   if typ == "exact" :
-      try:
-         if n <= 0:
-           n = 1
-         l = len(res[u"list"])
-         res = res[u"list"][n - 1][u"definition"]
-         res = res.replace("\r\n", " ")
-         if l > 1 and n < l:
+    res = json.load(res)
+    try:
+        if n <= 0:
+          n = 1
+        l = len(res[u"list"])
+        res = res[u"list"][n - 1][u"definition"]
+        res = res.replace("\r\n", " ")
+        if l > 1 and n < l:
             mylib.print_console("%d found '.ud %s %d' for the next one " % (l,q,n+1))
-         mylib.print_console("%s" % res)
-      except IndexError :
-         mylib.print_console("Not found")
-   else:
-      mylib.print_console("Not found")
+        mylib.print_console("%s" % res)
+    except IndexError :
+        mylib.print_console("Not found")
 else:
-    mylib.print_console(USAGE);
+     mylib.print_console(USAGE);
 # vim: ts=4:sw=4
